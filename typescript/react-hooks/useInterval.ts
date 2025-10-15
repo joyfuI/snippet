df92baea8cfb73b9db4ede577a27804cec6635c0
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-10-15 23:25:39 +0900

    useInterval에 useEffectEvent 도입

--- typescript/react-hooks/useInterval.ts
+++ typescript/react-hooks/useInterval.ts
@@ -1,17 +1,16 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useEffectEvent } from 'react';
 
 /**
  * setInterval 훅. setInterval과 다르게 최초에 callback을 한번 실행하고 반복함
- * @param callback 컴포넌트가 마운트 된 직후에 실행할 함수
+ * @param callback 반복 실행할 함수
  * @param ms ms마다 반복
  */
 const useInterval = (callback: () => void, ms: number) => {
-  const callbackRef = useRef(callback);
-  callbackRef.current = callback;
+  const func = useEffectEvent(callback);
 
   useEffect(() => {
-    const timer = setInterval(callbackRef.current, ms);
-    callbackRef.current();
+    const timer = setInterval(func, ms);
+    func(); // 즉시 1회 호출
     return () => clearInterval(timer);
   }, [ms]);
 };
