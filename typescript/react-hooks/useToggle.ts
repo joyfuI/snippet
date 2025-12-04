f9734ded7ff4779f74b7c889d3dd5d8f7ebb8220
joyfuI <1342862+joyfuI@users.noreply.github.com>
2025-12-05 01:48:07 +0900

    useToggle.ts 추가

---
+++ typescript/react-hooks/useToggle.ts
@@ -0,0 +1,16 @@
+import { useCallback, useState } from 'react';
+
+/**
+ * 토글 훅
+ * @param initialValue 초기값
+ * @returns [현재값, 토글함수, 설정함수]
+ */
+const useToggle = (initialValue: boolean | (() => boolean) = false) => {
+  const [value, setValue] = useState(initialValue);
+
+  const toggleValue = useCallback(() => setValue((prev) => !prev), []);
+
+  return [value, toggleValue, setValue] as const;
+};
+
+export default useToggle;
